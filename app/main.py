115cc60f902d4e371fe6f3f0e16b5b18ee42f31c
Florian Wesseler <fwesseler@phoenixcontact.com>
2024-02-07 18:27:49 +0100

    small changes

--- app/main.py
+++ app/main.py
@@ -27,9 +27,9 @@ def create_todo(toDoItem: schema.ToDoCreate, db: Session = Depends(get_db)):
     return service.create_toDoItem(db, toDoItem)
 
 @app.patch("/todos/{todo_id}", response_model=schema.ToDoItem)
-def update_todo(todo_id: str, toDoItem: schema.ToDoItemUpdate, db: Session = Depends(get_db)):
-    return service.update_toDoItem(db, int(todo_id), toDoItem)
+def update_todo(todo_id: int, toDoItem: schema.ToDoItemUpdate, db: Session = Depends(get_db)):
+    return service.update_toDoItem(db, todo_id, toDoItem)
 
 @app.delete("/todos/{todo_id}")
-def delete_todo(todo_id: str, db: Session = Depends(get_db)):
-    return service.delete_toDoItem(db, int(todo_id))
+def delete_todo(todo_id: int, db: Session = Depends(get_db)):
+    return service.delete_toDoItem(db, todo_id)
