192124749b0242a856bebca2cf88db872bc5cfc9
Florian Wesseler <fwesseler@phoenixcontact.com>
2024-02-19 15:05:15 +0100

    adjusted read unittest

--- tests/test_main.py
+++ tests/test_main.py
@@ -8,14 +8,15 @@ class TestTodos(unittest.TestCase):
     @patch("app.service.get_toDoItems")
     def test_read_todos_success(self, mock_get_toDoItems):
         mock_get_toDoItems.return_value = [
-            schema.ToDoItem(id=1, title="Test Todo 1", completed=False),
-            schema.ToDoItem(id=2, title="Test Todo 2", completed=True),
+            schema.ToDoItem(id=1, title="Test Todo 1", done=False),
+            schema.ToDoItem(id=2, title="Test Todo 2", done=True),
         ]
 
         response = main.read_todos(mock_get_toDoItems)
 
         # self.assertEqual(response.status_code, 200)
         self.assertEqual(response, mock_get_toDoItems.return_value)
+        assert response[1].done
 
     @patch("app.service.get_toDoItems")
     def test_read_one_todo_success(self, mock_get_toDoItems):
